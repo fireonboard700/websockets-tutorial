0ac63a4f00019f4a46b78f17c397482f904e5069
vritra <s.bindumadhavan@telusinternational.com>
2025-04-18 08:27:56 +0530

    "Deploy to Koyeb."

--- app.py
+++ app.py
@@ -2,12 +2,11 @@
 import asyncio
 from websockets.asyncio.server import serve
 from connect4 import Connect4
-
-
+import http
+import os
+import signal
 import json
-
 from connect4 import PLAYER1, PLAYER2
-
 import secrets
 
 
@@ -120,9 +119,16 @@ async def handler(websocket):
         # First player starts a new game.
         await start(websocket)
 
+def health_check(connection, request):
+    if request.path == "/healthz":
+        return connection.respond(http.HTTPStatus.OK, "OK\n")
+
 async def main():
-    async with serve(handler, "", 8001) as server:
-        await server.serve_forever()
+    port = int(os.environ.get("PORT", "8001"))
+    async with serve(handler, "", port, process_request=health_check) as server:
+        loop = asyncio.get_running_loop()
+        loop.add_signal_handler(signal.SIGTERM, server.close)
+        await server.wait_closed()
 
 
 if __name__ == "__main__":
